2b8dc300620e2699f3bf5360babd3774957dc373
Julia Santi <juliagabriela98@gmail.com>
2024-10-13 21:04:25 -0300

    feat: adding api call to get dog breeds

--- src/components/DogList/DogList.jsx
+++ src/components/DogList/DogList.jsx
@@ -1,34 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DogCard from "../DogCard/DogCard";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./DogList.module.css";
-import dogs from "../../service/mock/dogsData";
+import { fetchDogBreeds } from "../../service/dogService.js";
 
 const DogList = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [dogs, setDogs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const filteredDogs = dogs.filter((dog) =>
-    dog.breed.toLowerCase().includes(searchTerm.toLowerCase().trim())
+    dog.attributes.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
   );
+
+  useEffect(() => {
+    const getDogBreeds = async () => {
+      try {
+        setLoading(true);
+        const dogBreeds = await fetchDogBreeds();
+        setDogs(dogBreeds);
+      } catch (error) {
+        console.error("Failed to fetch dog breeds:", error);
+      }
+      setLoading(false);
+    };
+
+    getDogBreeds();
+  }, []);
 
   return (
     <div className={styles.wrapper}>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <div className={styles.container}>
-        {filteredDogs.length > 0 ? (
+        {filteredDogs.length > 0 &&
           filteredDogs.map((dog, index) => (
             <DogCard
               key={index}
-              breed={dog.breed}
-              description={dog.description}
-              image={dog.image}
+              breed={dog.attributes.name}
+              description={dog.attributes.description}
+              image={dog.attributes.image}
             />
-          ))
-        ) : (
+          ))}
+
+        {!loading && !filteredDogs.length && (
           <p className={styles.noResults}>
             No results found. Try another breed!
           </p>
         )}
+
+        {loading && <p className={styles.noResults}>Loading...</p>}
       </div>
     </div>
   );
