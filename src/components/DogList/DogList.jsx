d6810038295ed1f72ca1d3f9408d838cc6548f5f
Julia Santi <juliagabriela98@gmail.com>
2026-04-24 18:16:59 -0300

    feat: remove the code from dog list in code sketch

--- src/components/DogList/DogList.jsx
+++ src/components/DogList/DogList.jsx
@@ -5,50 +5,15 @@ import styles from "./DogList.module.css";
 import { fetchDogBreeds } from "../../service/dogService.js";
 
 const DogList = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [dogs, setDogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const filteredDogs = dogs.filter((dog) =>
-    dog.attributes.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
-  );
-
-  useEffect(() => {
-    const getDogBreeds = async () => {
-      try {
-        setLoading(true);
-        const dogBreeds = await fetchDogBreeds();
-        setDogs(dogBreeds);
-      } catch (error) {
-        console.error("Failed to fetch dog breeds:", error);
-      }
-      setLoading(false);
-    };
-
-    getDogBreeds();
-  }, []);
-
   return (
     <div className={styles.wrapper}>
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      {/* Search Bar */}
       <div className={styles.container}>
-        {filteredDogs.length > 0 &&
-          filteredDogs.map((dog, index) => (
-            <DogCard
-              key={index}
-              breed={dog.attributes.name}
-              description={dog.attributes.description}
-              image={dog.attributes.image}
-            />
-          ))}
+        {/* Dogs Cards */}
 
-        {!loading && !filteredDogs.length && (
-          <p className={styles.noResults}>
-            No results found. Try another breed!
-          </p>
-        )}
+        {/* Dogs noResults */}
 
-        {loading && <p className={styles.noResults}>Loading...</p>}
+        {/* Dogs loading */}
       </div>
     </div>
   );
